9dbfa1bd67ae89d26127376bc4d48077f303dc24
t3rmian <t3rmian@gmail.com>
2019-05-08 20:51:48 +0200

    Add noscript section

--- static.config.js
+++ static.config.js
@@ -72,33 +72,48 @@ export default {
     require.resolve("react-static-plugin-sass")
   ],
   Document: ({ Html, Head, Body, children }) => {
-    return(
-    <Html lang="x-default">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#b91d47" />
-        <meta name="msapplication-TileColor" content="#b91d47" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-      <Body>{children}</Body>
-    </Html>
-  )}
+    return (
+      <Html lang="x-default">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#b91d47" />
+          <meta name="msapplication-TileColor" content="#b91d47" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <Body>
+          <noscript>
+            <div
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%, -50%)"
+              }}
+            >
+              Please enable JavaScript to view the website
+            </div>
+          </noscript>
+          {children}
+        </Body>
+      </Html>
+    );
+  }
 };
